6ce6c10ae572ec708a62aa4345c18ab0538813c8
Krone <ahustinkrone@gmail.com>
2025-09-28 19:53:59 -0300

    Working sparks over cursor

--- public/shaders/sparks.glsl
+++ public/shaders/sparks.glsl
@@ -1,4 +1,3 @@
-const vec3 base_color = vec3(0.2, 0.3, 1.0);
 const vec3 blue_shift = vec3(1.0, 1.0, 1.0);
 
 uint pcg(uint v)
@@ -53,21 +52,47 @@ vec2 hash21(float p) {
 vec3 hash33(vec3 p3) {
     return vec3(pcg3d(uvec3(p3))) / 4294967296.;
 }
+vec2 norm(vec2 value, float isPosition) {
+    return (value * 2.0 - (iResolution.xy * isPosition)) / iResolution.y;
+}
+
+vec3 saturate(vec3 color, float factor) {
+    float gray = dot(color, vec3(0.299, 0.587, 0.114)); // luminance
+    return mix(vec3(gray), color, factor);
+}
 
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-    vec2 center = iResolution.xy / 2.;
+    vec3 base_color = iCurrentCursorColor.rgb;
+    base_color = vec3(0.1, 0.5, 2.5);
+    // base_color = vec3(0.5, 0.1, 2.5);
 
+    #if !defined(WEB)
+    fragColor = texture(iChannel0, fragCoord.xy / iResolution.xy);
+    #endif
+
+    float elapsed = iTime - iTimeCursorChange;
+
+    float duration = 0.2;
+    float fadeInTime = 0.06;
+    float fadeOutTime = 0.1;
+    float fadeIn = smoothstep(0.0, fadeInTime, elapsed);
+    float fadeOut = 1.0 - smoothstep(duration - fadeOutTime, duration, elapsed);
+    float fade = clamp(fadeIn * fadeOut, 0.0, 1.0);
+
+    vec2 center = norm(iCurrentCursor.xy, 1.);
+    vec2 vu = norm(fragCoord, 1.);
     float c0 = 0., c1 = 0.;
 
     for (float i = 0.; i < 50.; ++i) {
-        float t = 4. * iTime + hash11(i);
-
+        float t = 5. * iTime + hash11(i);
         vec2 v = hash21(i + 50. * floor(t));
         t = fract(t);
         v = vec2(sqrt(-2. * log(1. - v.x)), 6.283185 * v.y);
         v = 20. * v.x * vec2(cos(v.y), sin(v.y));
 
         vec2 p = center + t * v - fragCoord;
+        p.x = p.x + iCurrentCursor.x + iCurrentCursor.z * 0.5;
+        p.y = p.y + iCurrentCursor.y - iCurrentCursor.w * 0.5;
         c0 += 4. * (1. - t) / (1. + 0.3 * dot(p, p));
 
         p = p.yx;
@@ -76,16 +101,23 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
                 p.x / v.x,
                 p.y - p.x / v.x * v.y
             );
-
-        //float a = abs(p.x) < 0.1 ? 50./abs(v.x) : 0.;
-        //float b0 = max(2. - abs(p.y), 0.);
-        //float b1 = 0.2/(1.+0.0001*p.y*p.y);
-        //c0 += (1.-t)*b0*a;
-        //c1 += (1.-t)*b1*a;
     }
 
+    // vec3 rgb = c0 * base_color + c1 * base_color * blue_shift;
+    // rgb += hash33(vec3(fragCoord, iTime * 256.)) / 512.;
+    // rgb = pow(rgb, vec3(0.4545));
+    //
+    // // Apply fade factor
+    // rgb *= fade;
+    //
+    // fragColor = mix(vec4(rgb, 1.), fragColor, 0.5);
     vec3 rgb = c0 * base_color + c1 * base_color * blue_shift;
     rgb += hash33(vec3(fragCoord, iTime * 256.)) / 512.;
-    rgb = pow(rgb, vec3(0.4545));
-    fragColor = vec4(rgb, 1.);
+    //rgb = pow(rgb, vec3(0.4545));
+
+    float mask = clamp(c0 * 0.2, 0.0, 1.0) * fade;
+
+    // fragColor = mix(fragColor, vec4(rgb, 1.0), mask);
+    fragColor = fragColor + vec4(rgb * mask, 1.0); // additive
+    fragColor = min(fragColor, 1.0); // clamp
 }
