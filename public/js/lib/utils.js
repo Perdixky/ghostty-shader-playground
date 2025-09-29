bee748f2264270de83de5ab9fe630dad25686ffa
Krone <ahustinkrone@gmail.com>
2025-09-28 22:32:55 -0300

    remove not standard method substr

--- public/js/lib/utils.js
+++ public/js/lib/utils.js
@@ -1,6 +1,6 @@
 export function hexToRgbNormalized(hex) {
-  let r = parseInt(hex.substr(1, 2), 16) / 255;
-  let g = parseInt(hex.substr(3, 2), 16) / 255;
-  let b = parseInt(hex.substr(5, 2), 16) / 255;
+  let r = parseInt(hex.slice(1, 3), 16) / 255;
+  let g = parseInt(hex.slice(3, 5), 16) / 255;
+  let b = parseInt(hex.slice(5, 7), 16) / 255;
   return [r, g, b, 1];
 }
